60ffcf72b45e0c3fcc6026036288c81ef7b5795e
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2025-04-10 11:54:04 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub from Java!");
+        System.out.println("This change was made on GitHub!");
     }
 }
